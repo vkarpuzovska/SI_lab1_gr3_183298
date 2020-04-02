694816f9fa4902637002bfda14c545fd317c14d6
vkarpuzovska <62563501+vkarpuzovska@users.noreply.github.com>
2020-04-02 21:11:32 +0200

    taxReturn method finished

--- Item.java
+++ Item.java
@@ -1,15 +1,78 @@
+enum Tax
+{
+	A=18;
+	B=5;
+	C=0;
+
+}
 class Item {
 	int id;
 	String name;
 	double price;
 
 	//TODO add variable.
-    double tax;
+    Tax tax;
 	//TODO constructor
+	public Item(int id; String name; double price; Tax tax)
+	{
+		this.id=id;
+		this.name=name;
+		this.price=price;
+		this.tax=tax;
+	}
 
 	//TODO setters and getters
 
-	double taxReturn () {
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setTax(Tax tax) {
+		this.tax = tax;
+	}
+
+	public Tax getTax() {
+		return tax;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+
+
+	double taxReturn (double price, Tax tax) {
 		//TODO
+		double ddv=0;
+		switch(tax)
+		{ case A:
+				ddv=(tax/100)*price;
+				return 0.15*ddv;
+			case B:
+				ddv=(tax/100)*price;
+				return 0.15*ddv;
+
+			case C:
+				return ddv=price;
+			return 0.15*ddv;
+
+		}
 	}
 }
